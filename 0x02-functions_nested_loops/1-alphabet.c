57cc57068e082566aeb593774944785f081935f7
adanhazi <adan.hazi10@gmail.com>
2022-09-13 09:02:52 -0700

    Task one trial

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ /**
+  *print alhphabet in lower case followed 
+  *by a new line
+  *
+  *Return: Always 0 (Success)
+  */
+
+void print_alphabet(void)
+{
+  char alpha;
+
+  for (alpha = 'a'; alpha <= 'z'; alpha++)
+    {
+
+      _putchar(alpha);
+
+    }
+
+  _putchar('\n');
+
+}
