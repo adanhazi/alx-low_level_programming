85af081d2ce12451a996b56594f814339945224c
adanhazi <110994904+adanhazi@users.noreply.github.com>
2022-09-13 19:52:22 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,12 +8,10 @@
 
 void print_alphabet(void)
 {
-        char alpha;
-
-        for (alpha = 'a'; alpha <= 'z'; alpha++)
-        {
-               _putchar(alpha);
-        }
-        _putchar('\n');
-
+char alpha;
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
+_putchar(alpha);
+}
+_putchar('\n');
 }
